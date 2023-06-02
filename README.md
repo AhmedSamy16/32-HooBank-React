c3bfa7a19b91733c4894ff242ddba40e3c5f2640
Ahmed_Samy <ahmed.samy1640@gmail.com>
2023-06-03 02:30:00 +0300

    Project 32 completed

---
+++ README.md
@@ -0,0 +1,6 @@
+# 32-HooBank-React
+
+This is projet 32/50 of my React projects series. It's a design implemented in React and Tailwindcss.
+
+## Topics covered
+React.js, Tailwindcss
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+
+// eslint-disable-next-line react/prop-types
+const Button = ({ styles }) => {
+    return (
+        <button type="button" className={`py-4 px-6 bg-blue-gradient rounded-xl ${styles} font-poppins font-medium text-[18px] text-primary outline-none`}>Get Started</button>
+    )
+}
+
+export default Button
